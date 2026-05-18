4ec502c2979e7fa45eddd6e25e5f18830ae1b53e
ichul <ikizaki01@gmail.com>
2026-05-18 15:35:47 +0700

    update

---
+++ modul5_perulanganPHP/tugas_2.php
@@ -0,0 +1,6 @@
+<?php
+$bilangan = 2;
+for ($a=2; $a<=50; $a++){
+    echo "Bilangan Sekarang adalah $bilangan <br>"; $bilangan += $a;
+}
+?>
---
+++ modul5_perulanganPHP/tugas_3.php
@@ -0,0 +1,7 @@
+<?php
+$bil = 3;
+while ($bil <= 127){
+    if($bil %6 == 0) echo $bil. "<br />";
+    $bil++;
+}
+?>
